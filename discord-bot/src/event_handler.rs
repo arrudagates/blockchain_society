cf6bf891f88bbcd15f6fb0d76ab9f93106ab3187
S0raWasTaken <s0ra@duck.com>
2022-01-17 18:28:31 -0500

    Add event matching and decoding

--- discord-bot/src/event_handler.rs
+++ discord-bot/src/event_handler.rs
@@ -1,9 +1,14 @@
-use crate::primitives::Error;
+use crate::primitives::{Error, MemberBanned};
+use codec::Decode;
 use subxt::{ClientBuilder, DefaultConfig, DefaultExtra, EventSubscription};
 use tokio::spawn;
 
 #[subxt::subxt(runtime_metadata_path = "../blockchain/metadata.scale")]
 pub mod polkadot {}
+
+type MemberAdded = polkadot::discord::events::MemberAdded;
+type RoleCreated = polkadot::discord::events::RoleCreated;
+type RoleAssigned = polkadot::discord::events::RoleAssigned;
 
 pub async fn handler() -> Result<(), Error> {
     env_logger::init();
@@ -27,12 +32,65 @@ pub async fn handler() -> Result<(), Error> {
         let decoder = api.client.events_decoder();
         let mut sub = EventSubscription::<DefaultConfig>::new(sub, decoder);
         loop {
-            let raw = sub.next().await.unwrap().unwrap();
-            if raw.pallet == String::from("Discord") {
-                todo!()
+            match sub.next().await {
+                Some(sub_result) => match sub_result {
+                    Ok(raw) => {
+                        if raw.pallet == String::from("Discord") {
+                            match raw.variant.as_str() {
+                                "BotAdded" => println!("{:?}", raw),
+                                "MemberBanned" => member_banned(
+                                    <MemberBanned as Decode>::decode(&mut &raw.data[..]).unwrap(),
+                                )
+                                .await
+                                .unwrap(),
+                                "RoleCreated" => role_created(
+                                    <RoleCreated as Decode>::decode(&mut &raw.data[..]).unwrap(),
+                                )
+                                .await
+                                .unwrap(),
+                                "RoleAssigned" => role_assigned(
+                                    <RoleAssigned as Decode>::decode(&mut &raw.data[..]).unwrap(),
+                                )
+                                .await
+                                .unwrap(),
+                                "MemberAdded" => member_added(
+                                    <MemberAdded as Decode>::decode(&mut &raw.data[..]).unwrap(),
+                                )
+                                .await
+                                .unwrap(),
+                                _ => todo!(),
+                            }
+                        }
+                    }
+                    Err(why) => eprintln!("{:?}", Error::Subxt(why)),
+                },
+                _ => eprintln!(
+                    "{:?}",
+                    Error::Custom(String::from("Empty raw event received from socket"))
+                ),
             }
         }
     });
 
+    Ok(())
+}
+
+async fn member_banned(event: MemberBanned) -> Result<(), Error> {
+    println!("{:?}", event.0);
+    Ok(())
+}
+
+async fn member_added(event: MemberAdded) -> Result<(), Error> {
+    println!("{:?}", event.0);
+    Ok(())
+}
+
+async fn role_created(event: RoleCreated) -> Result<(), Error> {
+    println!("{:?}", event.0);
+    Ok(())
+}
+
+async fn role_assigned(event: RoleAssigned) -> Result<(), Error> {
+    println!("{:?}", event.0);
     Ok(())
 }
