7a2fc5625375a1d332c06a2f6323668b1b72538d
S0raWasTaken <s0ra@duck.com>
2022-01-18 03:12:51 -0500

    Add `Serenity` and `Utf8` custom errors

--- discord-bot/src/primitives.rs
+++ discord-bot/src/primitives.rs
@@ -1,7 +1,8 @@
-use std::{env::VarError, fmt::Debug, num::ParseIntError};
+use std::{env::VarError, fmt::Debug, num::ParseIntError, string::FromUtf8Error};
 
 use codec::Decode;
 use dotenv::Error as DotenvError;
+use serenity::Error as SerenityError;
 use subxt::{sp_runtime::AccountId32, Error as SubxtError};
 
 #[derive(Debug)]
@@ -12,6 +13,8 @@ pub enum Error {
     Client(String),
     Subxt(SubxtError),
     Custom(String),
+    Serenity(SerenityError),
+    UTF8(FromUtf8Error),
 }
 
 #[derive(Decode)]
