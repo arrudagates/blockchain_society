2b1b3fb4e213155d349dda4f87d8069a5c5a3e9d
S0raWasTaken <s0ra@duck.com>
2022-01-21 05:19:49 -0500

    Add more error kinds

--- discord-bot/src/primitives.rs
+++ discord-bot/src/primitives.rs
@@ -1,6 +1,6 @@
 use std::{env::VarError, fmt::Debug, num::ParseIntError, string::FromUtf8Error};
 
-use codec::Decode;
+use codec::{Decode, Error as DecodeError};
 use dotenv::Error as DotenvError;
 use serenity::Error as SerenityError;
 use subxt::{sp_runtime::AccountId32, Error as SubxtError};
@@ -15,7 +15,11 @@ pub enum Error {
     Custom(String),
     Serenity(SerenityError),
     UTF8(FromUtf8Error),
+    Decode(DecodeError),
 }
 
+// TEMP manually reimplement some events due to decoding issues with Discord IDs
 #[derive(Decode)]
 pub struct MemberBanned(pub AccountId32, pub u64, pub Vec<u8>);
+#[derive(Decode)]
+pub struct RoleAssigned(pub AccountId32, pub u64, pub Vec<u8>);
